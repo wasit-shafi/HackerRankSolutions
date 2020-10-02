5eb7614a00f1d1cb41e6c01716115667b2f40fed
Tairah Andrabi <56019029+tairahandrabi@users.noreply.github.com>
2020-10-02 23:34:10 +0530

    Add files via upload

--- c/Sum and Difference of Two Numbers.c
+++ c/Sum and Difference of Two Numbers.c
@@ -6,42 +6,6 @@ int main()
     float f1, f2;
     
     scanf("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);#include <stdio.h>
-
-int main()
-{
-	int n1, n2;
-    float f1, f2;
-    
-    scanf("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);#include <stdio.h>
-
-int main()
-{
-	int n1, n2;
-    float f1, f2;
-    
-    scanf("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);#include <stdio.h>
-
-int main()
-{
-	int n1, n2;
-    float f1, f2;
-    
-    scanf("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
-    scanf("%f %f", &f1, &f2);("%d %d", &n1, &n2);
     scanf("%f %f", &f1, &f2);
     printf("%d %d\n", n1 + n2, n1 - n2);
     printf("%.1f %.1f", f1 + f2, f1 - f2);
